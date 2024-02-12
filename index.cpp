e568dc31d691e26f97b020d3fffaa7d90b6f9423
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-02-12 19:37:06 +0530

    hello

---
+++ index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cout << "enter value a: ";
+    cin >> a;
+    cout << "enter value b : ";
+    cin >> b;
+
+    c=a+b;
+    cout <<"the sum is : " << c;
+    return 0;
+}
